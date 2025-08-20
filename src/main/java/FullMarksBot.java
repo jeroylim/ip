9027305ca1b498d09d860860d4db0052a1abfa91
jeroylim <138494063+jeroylim@users.noreply.github.com>
2025-08-20 15:12:48 +0800

    Level 2. Add, List

--- src/main/java/FullMarksBot.java
+++ src/main/java/FullMarksBot.java
@@ -1,21 +1,36 @@
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.List;
 
 public class FullMarksBot {
     public static String NAME = "FullMarksBot";
 
     public static void main(String[] args) {
         System.out.printf("Hello, I'm %s," +
-                "the bot that gives you full marks", NAME);
+                "the bot that gives you full marks, " +
+                "please write down what you want me to store!", NAME);
 
         Scanner scanner = new Scanner(System.in);
         String input;
+        ArrayList<String> tasks = new ArrayList<>();
         while (true) {
             input = scanner.nextLine();
+
+            if (input.equals("list")) {
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.println((i+1) + ": " + tasks.get(i));
+                }
+                continue;
+            }
+
             if (input.equals("bye")) {
                 System.out.println("bye bye for now!");
                 break;
             }
-            System.out.println("You just said: " + input);
+
+            tasks.add(input);
+            System.out.println("New Task: " + input);
+
         }
     }
 }
