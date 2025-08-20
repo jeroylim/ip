023d9ddebe810e61897cfce8562f3b2467bbb366
jeroylim <138494063+jeroylim@users.noreply.github.com>
2025-08-20 14:32:38 +0800

    Level 0. Rename, Greet, Exit

---
+++ src/main/java/FullMarksBot.java
@@ -0,0 +1,9 @@
+public class FullMarksBot {
+    public static String NAME = "FullMarksBot";
+
+    public static void main(String[] args) {
+        System.out.printf("Hello, I'm %s," +
+                "the bot that gives you full marks!" +
+                " Okay bye.", NAME);
+    }
+}
