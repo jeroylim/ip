894b602d98e8e8cc4c3f712ba64462690ba4d8e5
jeroylim <138494063+jeroylim@users.noreply.github.com>
2025-08-20 14:46:05 +0800

    Level 1. Echo

--- src/main/java/FullMarksBot.java
+++ src/main/java/FullMarksBot.java
@@ -1,9 +1,21 @@
+import java.util.Scanner;
+
 public class FullMarksBot {
     public static String NAME = "FullMarksBot";
 
     public static void main(String[] args) {
         System.out.printf("Hello, I'm %s," +
-                "the bot that gives you full marks!" +
-                " Okay bye.", NAME);
+                "the bot that gives you full marks", NAME);
+
+        Scanner scanner = new Scanner(System.in);
+        String input;
+        while (true) {
+            input = scanner.nextLine();
+            if (input.equals("bye")) {
+                System.out.println("bye bye for now!");
+                break;
+            }
+            System.out.println("You just said: " + input);
+        }
     }
 }
