c80fd52845536f0be9f9ae5c0f46e7eb9d8e1db9
jeroylim <138494063+jeroylim@users.noreply.github.com>
2025-08-27 00:37:43 +0800

    Add Ui class

---
+++ src/main/java/Ui.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Ui {
+    private final Scanner scanner;
+
+    public Ui() {
+        this.scanner = new Scanner(System.in);
+    }
+
+    public void showWelcome(String NAME) {
+        System.out.printf("Hello, I'm %s, the bot that gives you full marks, please write down what you want me to store!%n", NAME);
+    }
+
+    public String readCommand() {
+        return scanner.nextLine();
+    }
+
+    public void showTaskList(TaskList taskList) {
+        for (int i = 0; i < taskList.size(); i++) {
+            FullMarksBot.Task t = taskList.getTask(i);
+            System.out.println((i + 1) + ": " + t.getStatusIcon() + t.getDescription());
+        }
+    }
+
+    public void showMessage(String msg) {
+        System.out.println(msg);
+    }
+
+    public String ask(String prompt) {
+        System.out.println(prompt);
+        return scanner.nextLine();
+    }
+}
