2cea952217ee7ecf49aa58600263a5d28bcd248f
jeroylim <138494063+jeroylim@users.noreply.github.com>
2025-08-28 01:05:18 +0800

    Add JUnit Tests

---
+++ src/test/java/fullMarksBot/TaskListTest.java
@@ -0,0 +1,51 @@
+package fullMarksBot;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+class TaskListTest {
+
+    private TaskList taskList;
+
+    @BeforeEach
+    void setUp() {
+        taskList = new TaskList();
+    }
+
+    @Test
+    void testAddTask() {
+        FullMarksBot.Task task = new FullMarksBot.Todo("Eat");
+        taskList.addTask(task);
+
+        assertEquals(1, taskList.size());
+        assertEquals(task, taskList.getTask(0));
+    }
+
+    @Test
+    void testDeleteTask() {
+        FullMarksBot.Task task1 = new FullMarksBot.Todo("Write a lot");
+        FullMarksBot.Task task2 = new FullMarksBot.Todo("Submit assignment");
+
+        taskList.addTask(task1);
+        taskList.addTask(task2);
+
+        try {
+            taskList.deleteTask(0);
+        } catch (FullMarksBot.FullMarksException e) {
+            fail("Deleting a valid task should not throw an exception");
+        }
+
+        assertEquals(1, taskList.size());
+        assertEquals(task2, taskList.getTask(0));
+
+        try {
+            taskList.deleteTask(5);
+            fail("Expected FullMarksException was not thrown");
+        } catch (FullMarksBot.FullMarksException e) {
+            assertEquals("Task number 6 does not exist.", e.getMessage());
+        }
+    }
+}
